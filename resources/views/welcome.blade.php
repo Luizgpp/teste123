6c35f02a574dcca2b2eea56a9eeb771b03e12a3a
Your NameRafael <rafastdb@hotmail.com>
2017-08-03 19:13:22 -0300

    copia gabriel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>muda</title>
-
-        <!-- Fonts -->
+        <title>City tourr</title> 
+       <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
@@ -79,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    City tour
                 </div>
 
                 <div class="links">
