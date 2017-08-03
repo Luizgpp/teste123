cca9a9d2037e26213b7fce9ccbd7d67526630f61
Luiz Gabriel <luizgpp@gmail.com>
2017-08-02 23:20:15 -0300

    Mudança de layout, paginas html, css e js

---
+++ resources/views/partials/_javascript.blade.php
@@ -0,0 +1,41 @@
+		<!-- JS -->
+		
+		<!-- jquery js -->
+        <script src="{{ asset('js/vendor/jquery-1.12.0.min.js') }}"></script>
+		
+		<!-- price-slider js -->
+        <script src="{{ asset('js/price-slider.js') }}"></script>		
+		
+		<!-- bootstrap js -->
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+		
+		<!-- owl.carousel.min js -->
+        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+		
+		<!-- jquery.scrollUp js -->
+        <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
+		
+		<!-- mixitup js -->
+        <script src="{{ asset('js/jquery.mixitup.min.js') }}"></script>
+
+		<!-- fancybox js -->
+		<script src="{{ asset('js/fancybox/jquery.fancybox.pack.js') }}"></script>	
+
+		<!-- jquery-counterup js -->
+        <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+        <script src="{{ asset('js/waypoints.min.js') }}"></script>	
+
+		<!-- jquery-meanmenu js -->
+		<script src="{{ asset('js/jquery.meanmenu.js') }}"></script>
+		
+		<!-- RS-Plugin JS
+		============================================ -->
+		<script type="text/javascript" src="{{ asset('js/jquery.themepunch.tools.min.js') }}"></script>   
+		<script type="text/javascript" src="{{ asset('js/jquery.themepunch.revolution.min.js') }}"></script>
+		<script src="{{ asset('js/rs.home.js') }}"></script>		
+		
+		<!-- plugins js -->
+        <script src="{{ asset('js/plugins.js') }}"></script>
+		
+		<!-- main js -->
+        <script src="{{ asset('js/main.js') }}"></script>
